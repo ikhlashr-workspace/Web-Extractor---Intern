043060245d7094ff7dda789b3592db3f75f5c299
antasetyawan-work <work.antasetyawan@gmail.com>
2024-09-05 06:23:34 +0700

    Check Data Button

--- obj/Release/net8.0-windows/win-x64/WebDataExtractor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/WebDataExtractor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gaotekmagang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09dd3f0831487e14cf40c3c0a9f31a297cebcb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01bdeb8080959b0737be6ce9c1d1a478ae798c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("gaotekmagang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gaotekmagang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
